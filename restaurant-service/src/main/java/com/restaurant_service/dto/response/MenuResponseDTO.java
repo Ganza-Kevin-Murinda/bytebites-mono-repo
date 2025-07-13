64d3b6d2a3e2083e830380ab17842a6ba1103424
kevin <murindakevinmulk@gmail.com>
2025-07-13 12:45:05 +0200

    feat: Add menu id in MenuResponseDTO

--- restaurant-service/src/main/java/com/restaurant_service/dto/response/MenuResponseDTO.java
+++ restaurant-service/src/main/java/com/restaurant_service/dto/response/MenuResponseDTO.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class MenuResponseDTO {
+    Long id;
     String name;
     double price;
     RestaurantSummaryDTO restaurantSummaryDto;
--- restaurant-service/src/main/java/com/restaurant_service/util/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.restaurant_service.util;
-
-public enum ERole {
-    ROLE_ADMIN,
-    ROLE_CUSTOMER,
-    ROLE_RESTAURANT_OWNER,
-}
